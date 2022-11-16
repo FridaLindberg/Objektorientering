6016a73a05d178c055e59059bff084f8dffb0bd0
Frida Lindberg <fridalindberg03@gmail.com>
2022-11-16 14:08:46 +0100

    tcp

--- src/sprint4/TCPintro/Client.java
+++ src/sprint4/TCPintro/Client.java
@@ -1,34 +1,39 @@
 package sprint4.TCPintro;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Client {
-     int port = 55555;
-     String mess = "Hejhej";
+    int port = 55555;
+    String mess = "Hejhej";
 
 
-     public Client() throws UnknownHostException {
+    public Client() throws UnknownHostException {
 
-         InetAddress ip = InetAddress.getLocalHost();
+        InetAddress ip = InetAddress.getLocalHost();
 
-         try(Socket socket = new Socket(ip, port);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)){
+        try (Socket socket = new Socket(ip, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-             while(true){
-                 out.println(mess);
-                 Thread.sleep(3000);
-             }
+            String fromServer;
+
+            while (true) {
+                out.println(mess);
+                fromServer = in.readLine();
+                System.out.println(fromServer);
+                Thread.sleep(3000);
+            }
 
 
-
-         }
-         catch (Exception e){
-             e.printStackTrace();
-         }
-     }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) throws UnknownHostException {
         new Client();
--- src/sprint4/TCPintro/Server.java
+++ src/sprint4/TCPintro/Server.java
@@ -3,25 +3,29 @@ package sprint4.TCPintro;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
     int port = 55555;
+    String ack = "message received";
+    String hej = "Hej och välkommen";
 
-    public Server (){
-        try(ServerSocket serverSocket = new ServerSocket(port);
-            Socket socket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+    public Server() {
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket socket = serverSocket.accept();
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
+            out.println(hej);
             String temp = "";
-
-            while((temp = in.readLine()) != null){
+            while ((temp = in.readLine()) != null) {
                 System.out.println(temp);
+                out.println(ack + ": " + temp);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
 
         }
@@ -29,4 +33,5 @@ public class Server {
 
     public static void main(String[] args) {
         new Server();
-    }}
+    }
+}
